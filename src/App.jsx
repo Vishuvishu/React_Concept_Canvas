7903f29fd13123e0e3a47d80cb306dcae691bf6b
VishuVishu <98393037+Vishuvishu@users.noreply.github.com>
2024-11-09 12:40:06 +0530

    Animated modal

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Slider3 from './Components/3d_slider'; // Correct casing
+import Modal from './Components/Modal';
 // import './styles.css';
 
 // Define your Layout component here or import it
@@ -9,14 +10,18 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/"  element={<Slider3 />}>
+        <Route path="/" >
           <Route index element={<Slider3 />} />
+          <Route path="3dslider" element={<Slider3 />} />
+        </Route>
+        <Route path="/modals" >
+          <Route index element={<Modal />} />
           {/* <Route path="blogs" element={<Blogs />} />
           <Route path="contact" element={<Contact />} />
           <Route path="*" element={<NoPage />} /> */}
         </Route>
       </Routes>
-    </BrowserRouter>
+    </BrowserRouter >
   );
 }
 
---
+++ src/Components/Modal.jsx
@@ -0,0 +1,90 @@
+import React, { useState } from "react";
+import './styles/modal.css'
+export default function Modal() {
+
+    const [modalClass, setModalClass] = useState("");
+
+    const handleClick = (buttonId) => {
+        setModalClass(buttonId);
+        document.body.classList.add("modal-active");
+    };
+
+    const closeModal = () => {
+        setModalClass("out");
+        document.body.classList.remove("modal-active");
+    };
+    return (
+        <div>
+            {/* Modal */}
+            <div id="modal-container" className={modalClass} onClick={closeModal}>
+                <div className="modal-background">
+                    <div className="modal">
+                        <h2>I'm a Modal</h2>
+                        <p>Hear me roar.</p>
+                        <svg
+                            className="modal-svg"
+                            xmlns="http://www.w3.org/2000/svg"
+                            width="100%"
+                            height="100%"
+                            preserveAspectRatio="none"
+                        >
+                            <rect
+                                x="0"
+                                y="0"
+                                fill="none"
+                                width="226"
+                                height="162"
+                                rx="3"
+                                ry="3"
+                            ></rect>
+                        </svg>
+                    </div>
+                </div>
+            </div>
+
+            {/* Main Content */}
+            <div className="frame">
+                <header>
+                    {/* <h2>Save These</h2> */}
+                    <h1 >CSS Modals</h1>
+                    <h2 >with animations</h2>
+                </header>
+
+                {/* Button Grid */}
+                <div className="button-grid">
+                    <div className="button-item">
+                        <button type="button" onClick={() => handleClick("one")}>
+                            Unfolding
+                        </button>
+                    </div>
+                    <div className="button-item">
+                        <button type="button" onClick={() => handleClick("two")}>
+                            Revealing
+                        </button>
+                    </div>
+                    <div className="button-item">
+                        <button type="button" onClick={() => handleClick("six")}>
+                            Sketch
+                        </button>
+                    </div>
+                    <div className="button-item">
+                        <button type="button" onClick={() => handleClick("side")}>
+                            Side
+                        </button>
+                    </div>
+                    <div className="button-item">
+                        <button type="button" onClick={() => handleClick("five")}>
+                            Speedy
+                        </button>
+                    </div>
+                    <div className="button-item">
+                        <button type="button" onClick={() => handleClick("seven")}>
+                            Bond
+                        </button>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    );
+}
